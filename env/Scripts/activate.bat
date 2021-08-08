f152f69ceedefd9e1e403175be1a5e1f689c9d7a
Darija Stukaitė <65291573+darstu@users.noreply.github.com>
2021-08-08 20:26:28 +0300

    readme update, deployment testing

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\siemk\Desktop\homework\env
+set VIRTUAL_ENV=C:\Users\stukd\Desktop\homework\python-task\env
 
 if not defined PROMPT set PROMPT=$P$G
 
